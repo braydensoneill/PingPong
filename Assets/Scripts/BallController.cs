5d93c5666026257d5a291f0d965e3fc8b7f078bd
braydensoneill <braydensoneill@gmail.com>
2022-06-24 00:05:57 +0100

    Ball Rebound | Performance Update
    - The ball now rebounds off of obstacles appropriately
    - Added the option to repeatedly spawn/delete balls for testing/ future game modes
    - Physics calculations now performed in LateUpdate

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,7 +6,10 @@ public class BallController : MonoBehaviour
 {
     private enum Direction {Left = 0, Right = 1}
 
-    public GameObject newBall;
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject wallUpper;
+    public GameObject wallLower;
     public CharacterScore playerScoreScript;
     public CharacterScore enemyScoreScript;
 
@@ -15,8 +18,16 @@ public class BallController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float speedIncreasePerTick;
 
+    [SerializeField] private float currentAngle;
+    private float colliderPositionZ;
+    private float ballPositionZ;
+
+    public GameObject newBall;
     private Vector3 initialPosition;
     private Quaternion initialRotation;
+
+    private float timer;
+    private float deathTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +35,7 @@ public class BallController : MonoBehaviour
         playerScoreScript = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterScore>();
         enemyScoreScript = GameObject.FindGameObjectWithTag("Enemy").GetComponent<CharacterScore>();
 
-        gameObject.name = "Ball";               // Change name of a new ball to avoid trails of "(Clone)" in name
-        speed = initialSpeed;                   // Reset the speed to the initial speed 
-        currentDirection = Random.Range(0, 2);  // Return a avalue of either 0 or 1
+        InitialiseBall();
     }
 
     // Update is called once per frame
@@ -34,11 +43,79 @@ public class BallController : MonoBehaviour
     {
         IncreaseSpeed(speedIncreasePerTick);
 
+        //SpawnBallEveryXSeconds(2.5f);
+        //DestroyBallAfterXSeconds(5);
+    }
+
+    private void SpawnBallEveryXSeconds(float time)
+    {
+        timer += Time.deltaTime;
+
+        if (timer > time)
+        {
+            Instantiate(newBall, initialPosition, initialRotation);
+            timer = 0;
+        }
+    }
+
+    private void DestroyBallAfterXSeconds(float time)
+    {
+        deathTimer += Time.deltaTime;
+
+        if (deathTimer > time)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Movement();
+    }
+
+    private void ChangeRotationOnCollision(Collider other)
+    {
+        colliderPositionZ = other.gameObject.transform.position.z;
+        ballPositionZ = gameObject.transform.position.z;
+
+        float reboundAngle = colliderPositionZ - ballPositionZ;
+
+        if (other.gameObject.tag == "Player")
+            currentAngle = reboundAngle;
+
+        if (other.gameObject.tag == "Enemy")
+            currentAngle = -reboundAngle;
+
+        if (other.gameObject.tag == "WallUpper")
+            currentAngle *= -1;
+
+        if (other.gameObject.tag == "WallLower")
+            currentAngle *= -1;
+
+        RotateBall();
+    }
+
+    private void Movement()
+    {
         if (currentDirection == (int)Direction.Left)
             transform.Translate(Vector3.left * speed * Time.deltaTime);
 
         else if (currentDirection == (int)Direction.Right)
             transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void RotateBall()
+    {
+        float rotationStrength = 30;
+        transform.rotation = Quaternion.Euler(0, currentAngle * rotationStrength, 0);
+    }
+
+    private void InitialiseBall()
+    {
+        gameObject.name = "Ball";               // Change name of a new ball to avoid trails of "(Clone)" in name
+        speed = initialSpeed;                   // Reset the speed to the initial speed 
+        currentAngle = 0;
+        currentDirection = Random.Range(0, 2);  // Return a avalue of either 0 or 1
     }
 
     private void ResetBall()
@@ -55,10 +132,26 @@ public class BallController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
+        {
             currentDirection = (int)Direction.Right;
+            ChangeRotationOnCollision(other);
+        }
 
         if (other.gameObject.tag == "Enemy")
+        {
             currentDirection = (int)Direction.Left;
+            ChangeRotationOnCollision(other);
+        }
+
+        if (other.gameObject.tag == "WallUpper")
+        {
+            ChangeRotationOnCollision(other);
+        }
+
+        if (other.gameObject.tag == "WallLower")
+        {
+            ChangeRotationOnCollision(other);
+        }
 
         if (other.gameObject.tag == "GoalPlayer")
         {
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -18,7 +18,7 @@ public class CharacterController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (Input.GetKey(keycodeUp))
             MoveUp();
